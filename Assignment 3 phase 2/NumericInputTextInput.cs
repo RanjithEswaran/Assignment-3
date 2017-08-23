42847996fdf7adc6ee44bc62b530091012c38dfe
RanjithEswaran <30902121+RanjithEswaran@users.noreply.github.com>
2017-08-22 18:29:07 -0700

    Additional  assignment of week 3

---
+++ Assignment 3 phase 2/NumericInputTextInput.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericInputTextInput
+{
+    class TextInput
+    {
+        string stringValue;
+        public TextInput()
+        {
+            stringValue = "";
+        }
+        public virtual void Add(char c)
+        {
+            stringValue += c;
+        }
+        public virtual string GetValue()
+        {
+            return this.stringValue;
+        }
+    }
+    class NumericInput : TextInput
+    {
+        public override void Add(char c)
+        {
+            if (c >= '0' && c <= '9')
+                base.Add(c);
+        }
+        public override string GetValue()
+        {
+            return base.GetValue();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string Choice;
+                char c;
+                do {
+                    TextInput input = new NumericInput();
+
+                    do
+                    {
+                        Console.WriteLine("Enter the Character");
+                        c = Convert.ToChar(Console.ReadLine());
+                        input.Add(c);
+
+                        Console.WriteLine("Do you want to continue? Yes/No");
+                        Choice = Convert.ToString(Console.ReadLine());
+
+                    } while (Choice.Equals("Yes") || Choice.Equals("YES") ||
+                    Choice.Equals("yes") || Choice.Equals("Y") || Choice.Equals("y"));
+
+                    Console.WriteLine();
+                    Console.WriteLine("The given numeric string is {0}", input.GetValue());
+                    Console.WriteLine();
+
+                    Console.WriteLine("Do you want to continue typing new string ? Yes/No");
+                    Choice = Convert.ToString(Console.ReadLine());
+                } while (Choice.Equals("Yes") || Choice.Equals("YES") ||
+                    Choice.Equals("yes") || Choice.Equals("Y") || Choice.Equals("y"));
+
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Input");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Assignment 3 phase 2/RemoveDuplicateCharacters.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicateCharacterFromString
+{
+    class Program
+    {
+        public static string RemoveDuplicates(string TestString)
+        {
+            StringBuilder sb = new StringBuilder(TestString);
+            int index = 0, j;
+            for (int i = 0; i < TestString.Length; i++)
+            {
+                for (j = 0; j < i; j++)
+                {
+                    if (sb[i] == sb[j])
+                        break;
+                }
+                if (i == j)
+                    sb[index++] = sb[i];
+            }
+            TestString = sb.ToString().Substring(0, pos);
+            return TestString;
+        }
+        public static string RemoveDuplicate(string TestString)
+        {
+            string str="";
+            StringBuilder TestStringBuilder = new StringBuilder(TestString);
+            for (int i = 0; i < TestStringBuilder.Length - 1; i++)
+            {
+                for (int j = i + 1; j < TestStringBuilder.Length; j++)
+                {
+                    if (TestStringBuilder[i] != '\0' && TestStringBuilder[j] != '\0')
+                    {
+                        if (TestStringBuilder[i]== TestStringBuilder[j])
+                            TestStringBuilder[j] = '\0';
+                    }
+                }
+            }
+            for(int i=0;i<TestStringBuilder.Length;i++)
+            {
+                if (TestStringBuilder[i] != '\0')
+                    str += TestStringBuilder[i];
+            }
+            return str;
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                string TestString,Choice;
+                do {
+                    Console.WriteLine("Enter the string");
+                    TestString = Convert.ToString(Console.ReadLine());
+                    TestString = Program.RemoveDuplicate(TestString);
+                    Console.WriteLine();
+                    Console.WriteLine("The string after removing duplicates is {0}", TestString);
+                    Console.WriteLine();
+                    Console.WriteLine("Do you want to continue? Yes/No");
+                    Choice = Convert.ToString(Console.ReadLine());
+                } while (Choice.Equals("Yes")|| Choice.Equals("yes")||
+                Choice.Equals("YES")|| Choice.Equals("Y")|| Choice.Equals("y"));
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Input");
+            }
+                Console.ReadLine();
+            }
+        
+    }
+}
